8d9ac3f1f801bd50e26262840c5ce9f244d57a1a
Parapanov <59667950+Parapanov@users.noreply.github.com>
2020-03-08 12:00:09 +0200

    Add files via upload

---
+++ 1/Snake/src/GameBoard.java
@@ -0,0 +1,32 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameBoard extends JPanel implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 500 , HEIGHT = 500;
+	
+	
+	public GameBoard() {
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+	}
+	public void start() {
+		
+	}
+	public void stop() {
+		
+	}
+	public void tick() {
+		
+	}
+	public void paint(Graphics g) {
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
